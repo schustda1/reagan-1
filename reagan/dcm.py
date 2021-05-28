e7469b3dc1020e296797edf12893c07f88832280
Douglas Schuster <douglas.schuster@carat.com>
2021-05-28 10:20:56 -0400

    updating dcm api version

--- reagan/dcm.py
+++ reagan/dcm.py
@@ -9,7 +9,7 @@ from retrying import retry
 
 
 class DCMAPI(Subclass):
-    def __init__(self, networkId=8334, version = 'v3.4', verbose=0, profile_id=None, service_account_alias = None):
+    def __init__(self, networkId=8334, version = 'v3.5', verbose=0, profile_id=None, service_account_alias = None):
         super().__init__(verbose=verbose)
         self.version = version
         self.service_account_filepath = self.get_parameter_value(f'''/dcm/{"service_account_path" if not service_account_alias else service_account_alias}''')
